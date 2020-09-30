e9032a08937c7c066e2cf4ff9bc0ecf799df07db
Mikhaelle <mikhabueno@outlook.com>
2020-09-30 00:12:40 -0300

    spaw enemies in a visitor

--- Assets/Gameplay/Core/GameRoom.cs
+++ Assets/Gameplay/Core/GameRoom.cs
@@ -20,6 +20,13 @@ namespace Gameplay.Core
 
         Dealer Dealer { get; } = new Dealer();
 
+        public float maxTime = 50;
+        public float minTime = 10;
+        
+        private float time;
+
+     //The time to spawn the object
+        public float spawnTime;
         void Start()
         {
             var gameObjectFactory = new GameObjectFactory(cardPrefabMap, playerPrefab);
@@ -39,16 +46,34 @@ namespace Gameplay.Core
             Dealer.DealInitialCards(VisitorPlayer);
 
             matchReferee.Setup(gameActionFactory, players: new []{HomePlayer, VisitorPlayer});
+            
+            SetRandomTime();
+            time = minTime;
         }
+
+         void SetRandomTime(){
+         spawnTime = Random.Range(minTime, maxTime);
+         Debug.Log ("Next object spawn in " + spawnTime + " seconds.");
+     }
 
         void Awake() => AddObservers();
 
+        
         void OnDestroy() => RemoveObservers();
 
         void AddObservers()
         {
             Dealer.OnDealtCard += gameplayHUD.OnCardDealt;
             gameplayHUD.OnUseCard += OnHomePlayerUsedCard;
+        }
+
+        void Update(){
+            time += Time.deltaTime;
+
+            if(time >= spawnTime){
+                OnVisitorUsedCard(CardType.Warrior, 0);
+                time = 0;
+            }
         }
 
         void RemoveObservers()
@@ -59,8 +84,17 @@ namespace Gameplay.Core
 
         void OnHomePlayerUsedCard(CardType card, int laneIdx)
         {
+
             matchReferee.OnPlayerUsedCard(card, HomePlayer.Team, laneIdx);
             Dealer.DealCard(HomePlayer);
+            
+        }
+
+        void OnVisitorUsedCard(CardType card, int laneIdx){
+           
+            matchReferee.OnPlayerUsedCard(card, Team.Visitor, laneIdx);
+            Dealer.DealCard(VisitorPlayer);
+            
         }
     }
 }
